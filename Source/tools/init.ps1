a3b40cb65e9af971c5b4d5868753e5a7f311c446
Kristoffer Ahl <git@77dynamite.com>
2013-11-14 22:06:34 +0100

    - Allowing a config hashtable to be passed in to the init.ps1 file.

--- Source/tools/init.ps1
+++ Source/tools/init.ps1
@@ -1,10 +1,11 @@
 Param(
-	[Parameter(Position=0,Mandatory=1)] [hashtable]$parameters
+	[Parameter(Position=0,Mandatory=1)] [hashtable]$parameters,
+	[Parameter(Position=1,Mandatory=1)] [hashtable]$config
 )
 
 # -----------------------------------------------------
 # Put your initialization code here
 # -----------------------------------------------------
 
-# Outputs the parameters available to you
-$parameters | out-string
+$parameters		| out-string		# Outputs the parameters available to you
+$config			| out-string		# Outputs the config properties available to you
