58fac3492d7ff2d3488b47f2d8e07b82b89ac796
Kristoffer Ahl <git@77dynamite.com>
2013-11-14 20:19:39 +0100

    - Accepting hashtable parameter containing context related values. - Printing the available parameters.

--- Source/tools/init.ps1
+++ Source/tools/init.ps1
@@ -1,3 +1,10 @@
+Param(
+	[Parameter(Position=0,Mandatory=1)] [hashtable]$parameters
+)
+
 # -----------------------------------------------------
 # Put your initialization code here
 # -----------------------------------------------------
+
+# Outputs the parameters available to you
+$parameters | out-string
