88327af600ba3d7295051f609c38adf0efac61e1
Kristoffer Ahl <git@77dynamite.com>
2013-12-06 11:09:03 +0100

    - Added nuspec file as a template file instead and renamed it on install.

--- tools/install.ps1
+++ tools/install.ps1
@@ -7,3 +7,18 @@ Param(
 # -----------------------------------------------------
 
 $parameters		| out-string		# Outputs the parameters available to you
+
+
+
+# -----------------------------------------------------
+# Plugin scaffolding - This code can be removed.
+# -----------------------------------------------------
+
+$nuspecTemplate = "$($parameters.installationDir)\plugin.nuspec.template"
+$nuspec = ([xml] (get-content $nuspecTemplate))
+$nuspec.package.metadata.id = $parameters.pluginName
+$nuspec.save($nuspecTemplate)
+
+rename-item $nuspecTemplate -newname "$($parameters.pluginName).nuspec"
+
+# -----------------------------------------------------
